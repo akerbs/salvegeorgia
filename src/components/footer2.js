bb4253e28d4f3d31dc6a4f8f1e5933882a0c2f68
Anatol Kerbs <anker2702@gmail.com>
2020-11-21 16:46:26 +0300

    211120_17

--- src/components/footer2.js
+++ src/components/footer2.js
@@ -7,12 +7,25 @@ import InstagramIcon from "@material-ui/icons/Instagram"
 import { Link } from "gatsby"
 import { LanguageContext } from "./layout"
 
-const useStyles = makeStyles(theme => ({}))
+const useStyles = makeStyles(theme => ({
+  root: {
+    backgroundColor: theme.palette.primary.main,
+    // maxWidth: "100%",
+
+    padding: "5% 2% 0% 7%",
+
+    [theme.breakpoints.down("sm")]: {
+      padding: "18% 5% 0% 5%",
+    },
+  },
+}))
 
 export default function () {
+  const classes = useStyles()
+
   return (
-    <>
+    <div className={classes.root}>
       <div>© {new Date().getFullYear()}, salvegeorgia.com</div>
-    </>
+    </div>
   )
 }
