d71248dbaf306de4764b0cee5a74c365168e017a
Anatol Kerbs <anker2702@gmail.com>
2020-11-21 16:58:24 +0300

    211120_20

--- src/components/footer2.js
+++ src/components/footer2.js
@@ -1,29 +1,11 @@
-import React, { useContext } from "react"
-import Typography from "@material-ui/core/Typography"
-import Grid from "@material-ui/core/Grid"
-import { makeStyles } from "@material-ui/core/styles"
-import FacebookIcon from "@material-ui/icons/Facebook"
-import InstagramIcon from "@material-ui/icons/Instagram"
-import { Link } from "gatsby"
-import { LanguageContext } from "./layout"
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    // backgroundColor: theme.palette.primary.main,
-    // maxWidth: "100%",
-    // padding: "5% 2% 0% 7%",
-    // [theme.breakpoints.down("sm")]: {
-    //   padding: "18% 5% 0% 5%",
-    // },
-  },
-}))
+import React from "react"
 
 export default function () {
-  const classes = useStyles()
-
   return (
-    <div className={classes.root}>
-      <div>© {new Date().getFullYear()}, salvegeorgia.com</div>
+    <div style={{ backgroundColor: "tomato" }}>
+      fooooooter Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Veniam nesciunt repellendus reiciendis cupiditate a in deleniti fugiat
+      quia ducimus optio.
     </div>
   )
 }
