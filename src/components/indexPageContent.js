b7f0fffafd2f966258a66bc860c72ca2f13e3ed8
Anatol Kerbs <anker2702@gmail.com>
2020-11-25 15:56:54 +0300

    241120_18

--- src/components/indexPageContent.js
+++ src/components/indexPageContent.js
@@ -1,17 +1,14 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 
 import ComplexButtons from "./complexButtons"
 import { LanguageContext } from "./layout"
 import { HeaderHeightContext } from "./layout"
 
-const window = require("global/window")
-
 const useStyles = makeStyles(theme => ({
   contentWrapper: {
     padding: 0,
 
-    // minHeight: "100vh",
     // marginTop: "12vh",
     [theme.breakpoints.down("lg")]: {
       // marginTop: "7vh",
@@ -25,13 +22,8 @@ export default function IndexPageContent() {
   const { headerHeight } = useContext(HeaderHeightContext)
 
   return (
-    <div
-      className={classes.contentWrapper}
-      style={{
-        margin: 0,
-        marginTop: headerHeight,
-      }}
-    >
+    <div className={classes.contentWrapper} style={{ marginTop: headerHeight }}>
+      {/* <div id="top" style={{ margin: 0, padding: 0 }}></div> */}
       <ComplexButtons />
     </div>
   )
